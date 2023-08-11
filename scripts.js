a3cbb496f6934d8c82eb7dc02d48eaa39f7aca36
Transvita <133512369+Transvita@users.noreply.github.com>
2023-08-11 12:33:12 -0400

    Delete scripts.js

--- scripts.js
+++
@@ -1,7 +0,0 @@
-const sidebar = document.getElementById('sidebar');
-const content = document.getElementById('content');
-
-sidebar.addEventListener('click', () => {
-  sidebar.classList.toggle('active');
-  content.classList.toggle('active');
-});
